1a333710b9d332642a61f8240212f508e6a535f2
Guilhermee19 <guilherme.santana1998@gmail.com>
2023-03-01 20:56:23 -0300

    feat: carroseu dos projetos

--- src/app/components/carousel-projects/carousel-projects.component.ts
+++ src/app/components/carousel-projects/carousel-projects.component.ts
@@ -15,6 +15,65 @@ SwiperCore.use([EffectCoverflow]);
 export class CarouselProjectsComponent implements OnInit{
   constructor() { }
 
+  listProjects = [
+    {
+      name: "Tour Virtual",
+      image: "assets/pages/tour-virtual.png",
+      technology: "Angular",
+      hosted: "Vercel",
+      link: 'https://tcctourvirtual.vercel.app/home',
+      link_project: "https://github.com/Guilhermee19/TCC_Turismo_Virtual"
+    },
+    {
+      name: "Filmes e Series Marvel",
+      image: "assets/pages/movie-web.png",
+      technology: "React",
+      hosted: "Vercel",
+      link: 'https://react-movie-web-zeta.vercel.app//',
+      link_project: "https://github.com/Guilhermee19/react-movie-web"
+    },
+    {
+      name: "Gerador de Conselhos",
+      image: "assets/pages/advice-generator.png",
+      technology: "React",
+      hosted: "Vercel",
+      link: 'https://advice-generator-web.vercel.app/',
+      link_project: "https://github.com/Guilhermee19/advice-generator-web"
+    },
+    {
+      name: "Contagem Regressiva para o Ano Novo",
+      image: "assets/pages/countdown-timer.png",
+      technology: "HTML, CSS e JS Puro",
+      hosted: "Vercel",
+      link: 'https://countdown-timer-rosy.vercel.app/',
+      link_project: "https://github.com/Guilhermee19/countdown-timer"
+    },
+    {
+      name: "Linktree personalizado",
+      image: "assets/pages/bio-lm-web.png",
+      technology: "Angular",
+      hosted: "Vercel",
+      link: 'https://los-mundissas.vercel.app/',
+      link_project: "https://github.com/Guilhermee19/LM"
+    },
+    {
+      name: "PokeDex",
+      image: "assets/pages/pokedex-web.png",
+      technology: "Angular",
+      hosted: "Vercel",
+      link: 'https://pokedex-web-mocha.vercel.app/',
+      link_project: "https://github.com/Guilhermee19/pokedex-web"
+    },
+    {
+      name: "Rastreador de endereço IP",
+      image: "assets/pages/ip-address-tracker.png",
+      technology: "Angular",
+      hosted: "Vercel",
+      link: 'https://my-ip-address-tracker.vercel.app/',
+      link_project: "https://bitbucket.org/Guilhermee19/ip-address-tracker"
+    },
+  ]
+
   ngOnInit(): void {
   }
 }
