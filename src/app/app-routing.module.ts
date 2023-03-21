169db585f7e64f0db045b246ee965bacbe65e60b
Guilhermee19 <guilherme.santana1998@gmail.com>
2023-03-21 00:50:57 -0300

    feat: ajuste erro pagina não encontrada e ajustes no componente de apresentação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GamesComponent } from './pages/games/games.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
-  { path: '', component: NavbarComponent,
-    children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-    ]
-  },
+  { path: '', component: HomeComponent },
   { path: 'gamer', component: GamesComponent },
   { path: 'gamer/:name_game', component: GamesComponent },
   { path: '**', redirectTo: '' }
