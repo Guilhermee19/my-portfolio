bb15e9a4322bd5bd07ca4b82a58611d22da6f4e2
Guilhermee19 <guilherme@noclaftech.com>
2024-02-22 10:09:22 -0300

    feat: reset code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GamesComponent } from './pages/games/games.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'gamer', component: GamesComponent },
-  { path: 'gamer/:name_game', component: GamesComponent },
   { path: '**', redirectTo: '' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,24 +12,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { SwiperModule } from 'swiper/angular';
-import { GamesComponent } from './pages/games/games.component';
-import { MemoryGameComponent } from './pages/games/memory-game/memory-game.component';
-import { IconDirective } from './directives/icon.directive';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule } from '@angular/material/menu';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { NgxMaskModule } from 'ngx-mask';
 import { VerticalScrollComponent } from './components/vertical-scroll/vertical-scroll.component';
 import { GameBoyComponent } from './components/game-boy/game-boy.component';
-import { TicTacToeComponent } from './pages/games/tic-tac-toe/tic-tac-toe.component';
 import { SkillsComponent } from './components/home/skills/skills.component';
 import { AboutMeComponent } from './components/home/about-me/about-me.component';
 import { CarouselProjectsComponent } from './components/home/carousel-projects/carousel-projects.component';
 import { BlockComponent } from './components/block/block.component';
 import { LoadingPageComponent } from './components/loading-page/loading-page.component';
+import { IconDirective } from './directives/icon.directive';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+import { NgxMaskModule } from 'ngx-mask';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   declarations: [
@@ -38,11 +37,8 @@ import { LoadingPageComponent } from './components/loading-page/loading-page.com
     IconDirective,
     NavbarComponent,
     CarouselProjectsComponent,
-    GamesComponent,
-    MemoryGameComponent,
     VerticalScrollComponent,
     GameBoyComponent,
-    TicTacToeComponent,
     SkillsComponent,
     AboutMeComponent,
     BlockComponent,
