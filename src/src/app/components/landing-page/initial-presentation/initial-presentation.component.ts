5ae4c345c4bf57ab3c539adbefbec08ffd724a82
Guilhermee19 <guilherme@noclaftech.com>
2024-08-30 01:15:50 -0300

    feat: update angular/cli 17

---
+++ src/src/app/components/landing-page/initial-presentation/initial-presentation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-initial-presentation',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './initial-presentation.component.html',
+  styleUrl: './initial-presentation.component.scss'
+})
+export class InitialPresentationComponent {
+
+}
