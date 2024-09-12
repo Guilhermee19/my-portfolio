478a2011a68d4772d89b11b488c2a8dabf09fe07
Guilhermee19 <guilherme@noclaftech.com>
2024-09-12 01:33:10 -0300

    v-17

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./pages/landing-page/landing-page.component').then(m => m.LandingPageComponent) },
+  { path: '**', redirectTo: '' }
+];
